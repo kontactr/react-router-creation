fe6e3cf0c72376c19d00293ba2003dbf7dcc5954
Yash Gandhi <gandhiyash1381997@gmail.com>
2019-10-19 20:26:44 +0530

    better navigation for learning purpose in example
    more test cases for test different routes

---
+++ src/Components/MainNavigator.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { Link } from "../Router";
+import Header from "./Header";
+
+export default class MainNavigator extends React.Component {
+  render() {
+    return (
+      <>
+        {/*<Header hello={"World"} />*/}
+        <Link to={"/profile"}>
+          <div>Profile</div>
+        </Link>
+        <Link to={"/about"}>
+          <div>About</div>
+        </Link>
+      </>
+    );
+  }
+}
